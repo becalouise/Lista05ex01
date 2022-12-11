355c5c7b4cbb7df068f01e81a79cb8f4f251e749
Rebeca Louise <114708609+becalouise@users.noreply.github.com>
2022-12-11 14:46:51 -0300

    Exercicio 01

---
+++ Ex01.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Ex01 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Insira um número: ");
+        int numero=sc.nextInt();
+        sc.close();
+        imprimieLinha(numero);
+    }
+
+    public static void imprimieLinha(int numero){
+        int a, b;
+
+        for(a=1;a<=numero;a++){
+            for(b=1;b<=a;b++){
+                System.out.print(a+"   ");
+            }
+            System.out.println();
+        } 
+    }
+}
